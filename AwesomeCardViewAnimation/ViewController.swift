fd29a8477f4615764e869eac39ea1eda16c0a832
Baha Ganyev <ganyev7@gmail.com>
2018-10-31 15:34:01 +0600

    Initial Commit

---
+++ AwesomeCardViewAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwesomeCardViewAnimation
+//
+//  Created by Baha Ganyev on 31.10.2018.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
